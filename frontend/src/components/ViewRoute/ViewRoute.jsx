4c603037b198a26ef01772e9ed7bb2c69ff279db
Pranav Alva <pranav.alva@wexinc.com>
2024-11-10 23:59:49 -0500

    Add Custom Pins to Map

--- frontend/src/components/ViewRoute/ViewRoute.jsx
+++ frontend/src/components/ViewRoute/ViewRoute.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
+import { createRoot } from 'react-dom/client'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import axios from 'axios'
 import './ViewRoute.css'
+import { icons } from '../../constants'
+import PinBase from '../../assets/location-pin-solid'
 
 const ViewRoute = ({ tripId, arrivalDate }) => {
   const mapContainerRef = useRef(null)
@@ -20,7 +23,6 @@ const ViewRoute = ({ tripId, arrivalDate }) => {
     map.current = new maplibregl.Map({
       container: mapContainerRef.current,
       style: "https://api.maptiler.com/maps/basic-v2/style.json?key=oGOTJkyBZPxrLa145LN6",
-      // style: 'https://demotiles.maplibre.org/style.json',
       center: [139.753, 35.6844],
       zoom: 14,
     });
@@ -135,53 +137,129 @@ const ViewRoute = ({ tripId, arrivalDate }) => {
 
       // Add stops markers
       stops.forEach((stop) => {
-        console.log(stop.type_new)
         const el = document.createElement('div')
-        el.className = 'marker'
-        el.style.backgroundImage = `url(${getMarkerIcon(stop.type_new)})`
-        el.style.width = '32px'
-        el.style.height = '32px'
+        el.className = 'custom-marker'
 
-        new maplibregl.Marker(el)
+        const IconComponent = getMarkerIcon(stop.type_new)
+
+        const root = createRoot(el)
+        root.render(
+          <div className='pin-base'>
+            <PinBase 
+              fill={getMarkerColor(stop.type_new)}
+              width={30}
+              height={42}
+              style={{position: 'absolute', top: 0, left: 0}}
+            />
+            <div className='pin-icon'>
+            {IconComponent && 
+            <IconComponent
+              width={18}
+              height={18}
+              fill={getIconColor(stop.type_new)}
+              style={{position: 'absolute', top: 6}}
+            />}
+            </div>
+          </div>
+        )
+
+        // const base = document.createElement('div')
+        // base.className = 'pin-base'
+        // base.style.backgroundColor = getMarkerColor(stop.type_new)
+
+        // const icon = document.createElement('img')
+        // icon.src = getMarkerIcon(stop.type_new)
+        // icon.className = 'pin-icon'
+
+        // base.appendChild(icon)
+        // el.appendChild(base)
+
+        new maplibregl.Marker({element: el, anchor: 'bottom'})
           .setLngLat([stop.longitude, stop.latitude])
           .setPopup(new maplibregl.Popup().setHTML(`<h3>${stop.type_new}</h3>`))
           .addTo(mapInstance)
       })
-      
+
     }
 
   }, [mapInstance, trips, stops, loadingstops]);
-
-  console.log(trips)
-  console.log(stops)
 
   const getMarkerIcon = (type) => {
     // Return the icon based on the type
     switch (type) {
       case 'start':
-        return '/icons/start.png'
+        return icons.Home
       case 'end':
-        return '/icons/end.png'
+        return icons.Checkered
       case 'fuel':
-        return '/icons/fuel.png'
+        return icons.GasPump
       case 'docking':
-        return '/icons/docking.png'
+        return icons.Ramp
       case 'overnight rest':
-        return '/icons/overnight_rest.png'
+        return icons.Bed
       case 'short rest':
-        return '/icons/short_rest.png'
+        return icons.Meal
       case 'fuel_ext':
-        return '/icons/fuel_ext.png'
+        return icons.GasPump
       case 'warehouse':
-        return '/icons/warehouse.png'
+        return icons.Warehouse
       default:
-        return '/icons/default.png'
+        return icons.Flag
+    }
+  }
+
+  const getMarkerColor = (type) => {
+    switch (type) {
+      case 'start':
+        return '#FFA500'
+      case 'end':
+        return '#2E8B57'
+      case 'fuel':
+        return '#FFD700'
+      case 'fuel_ext':
+        return '#FFC107'
+      case 'docking':
+        return '#1E90FF'
+      case 'overnight rest':
+        return '#9370DB'
+      case 'warehouse':
+        return '#9370DB'
+      case 'short rest':
+        return '#1E90FF'    
+      default:
+        return '#1E90FF'
+    }
+  }
+
+  const getIconColor = (type) => {
+    switch (type) {
+      case 'start':
+        return '#995C00'
+      case 'end':
+        return '#1C5A3C'
+      case 'fuel':
+        return '#997300'
+      case 'fuel_ext':
+        return '#8F6A04'
+      case 'docking':
+        return '#0F4A7A'
+      case 'overnight rest':
+        return '#4D2A73'
+      case 'warehouse':
+        return '#4D2A73'
+      case 'short rest':
+        return '#0F4A7A'    
+      default:
+        return '#0F4A7A'
     }
   }
 
   return (
     <div className='map-wrap'>
-      <div ref={mapContainerRef} className='map'/>
+      <div ref={mapContainerRef} className='map' />
+      <PinBase 
+              fill={'#FFA500'}
+            /> 
     </div>
   )
 }
---
+++ frontend/src/constants/icons.js
@@ -0,0 +1,23 @@
+import Flag from '../assets/flag-solid.jsx';
+import Checkered from '../assets/flag-checkered-solid.jsx';
+import Dolly from '../assets/dolly-solid.jsx';
+import Bed from '../assets/bed-solid.jsx';
+import Ramp from '../assets/truck-ramp-box-solid.jsx';
+import Meal from '../assets/utensils-solid.jsx';
+import Warehouse from '../assets/warehouse-solid.jsx';
+import GasPump from '../assets/gas-pump-solid.jsx';
+import Home from '../assets/house-solid.jsx';
+
+const icons = {
+    Flag,
+    Checkered,
+    Dolly,
+    Bed,
+    Ramp,
+    Meal,
+    Warehouse,
+    GasPump,
+    Home
+}
+
+export default icons;
