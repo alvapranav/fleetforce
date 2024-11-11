176e6e26d8219ad36c538d7f102feedd0106ba04
Pranav Alva <pranav.alva@wexinc.com>
2024-11-10 20:08:08 -0500

    Render Map

--- frontend/src/components/ViewRoute/ViewRoute.jsx
+++ frontend/src/components/ViewRoute/ViewRoute.jsx
@@ -2,29 +2,32 @@ import React, { useEffect, useState, useRef } from 'react'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import axios from 'axios'
+import './ViewRoute.css'
 
 const ViewRoute = ({ tripId, arrivalDate }) => {
   const mapContainerRef = useRef(null)
+  const map = useRef(null)
   const [mapInstance, setMapInstance] = useState(null)
-  const [gpsPoints, setGpsPoints] = useState([]);
+  const [trips, setTrips] = useState(null);
   const [stops, setStops] = useState([]);
   const [toArrivalDate, setToArrivalDate] = useState('')
+  const [loadingtrips, setLoadingTrips] = useState(true)
+  const [loadingstops, setLoadingStops] = useState(true)
 
   useEffect(() => {
-    if (!mapContainerRef.current) return;
+    if (map.current) return;
 
-    const map = new maplibregl.Map({
+    map.current = new maplibregl.Map({
       container: mapContainerRef.current,
-      style: 'https://demotiles.maplibre.org/style.json',
-      center: [-96, 37.8],
-      zoom: 3,
+      style: "https://api.maptiler.com/maps/basic-v2/style.json?key=oGOTJkyBZPxrLa145LN6",
+      // style: 'https://demotiles.maplibre.org/style.json',
+      center: [139.753, 35.6844],
+      zoom: 14,
     });
 
-    map.addControl(new maplibregl.NavigationControl(), 'top-right');
+    map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
 
-    setMapInstance(map);
-
-    return () => map.remove();
+    setMapInstance(map.current);
   }, []);
 
   useEffect(() => {
@@ -32,62 +35,19 @@ const ViewRoute = ({ tripId, arrivalDate }) => {
 
     const fetchTripsData = async () => {
       try {
+        setLoadingTrips(true)
+
         const response = await axios.get(`/api/trips/${tripId}/${arrivalDate}`)
         const tripsData = response.data
-        setGpsPoints(tripsData)
 
-        const endDate = gpsPoints.to_arrival_datetime
-        setToArrivalDate(endDate);
+        setTrips(tripsData[0])
 
-        // Add route layer
-        gpsPoints.on('load', () => {
-          const routeGeoJson = {
-            type: 'FeatureCollection',
-            features: JSON.parse(gpsPoints[0].gps).map((point) => ({
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [point.Longitude_gps, point.Latitude_gps],
-              },
-              properties: {
-                timestamp: point.Dt,
-              },
-            })),
-          }
+        setLoadingTrips(false)
 
-
-          mapInstance.on('load', () => {
-
-            if (mapInstance.getSource('route')) {
-              mapInstance.getSource('route').setData(routeGeoJson)
-            } else {
-              mapInstance.addSource('route', {
-                type: 'geojson',
-                data: routeGeoJson,
-              })
-
-              mapInstance.addLayer({
-                id: 'route-points',
-                source: 'route',
-                type: 'circle',
-                paint: {
-                  'circle-color': 'gray',
-                  'circle-radius': 6,
-                },
-              })
-            }
-
-          })
-
-          const bounds = routeGeoJson.features.reduce((bounds, feature) => {
-            return bounds.extend(feature.geometry.coordinates)
-          }, new maplibregl.LngLatBounds(routeGeoJson.features[0].geometry.coordinates, routeGeoJson.features[0].geometry.coordinates))
-
-          mapInstance.fitBounds(bounds, { padding: 20 })
-        })
       }
       catch (error) {
-        console.error('Error fetching GPS data:', error)
+        console.error('Error fetching trips data:', error)
+        setLoadingTrips(false)
       }
     }
 
@@ -96,37 +56,103 @@ const ViewRoute = ({ tripId, arrivalDate }) => {
   }, [mapInstance, tripId, arrivalDate]);
 
   useEffect(() => {
-    if (!toArrivalDate || !mapInstance) return;
-
+    if (!mapInstance) return
     const fetchStopsData = async () => {
       try {
+
+        setLoadingStops(true)
+
+        const endDate = trips.to_arrival_datetime
+        setToArrivalDate(endDate);
+        console.log(toArrivalDate)
+
         const response = await axios.get(`/api/stops/${tripId}/${arrivalDate}/${toArrivalDate}`)
-        setStops(response.data)
+        const stopsData = response.data
+        setStops(stopsData)
 
-        console.log("hi")
+        setLoadingStops(false)
 
-        // Add stops markers
-        response.data.forEach((stop) => {
-          const el = document.createElement('div')
-          el.className = 'marker'
-          el.style.backgroundImage = `url(${getMarkerIcon(stop.type_new)})`
-          el.style.width = '32px'
-          el.style.height = '32px'
-
-          new maplibregl.Marker(el)
-            .setLngLat([stop.longitude, stop.latitude])
-            .setPopup(new maplibregl.Popup().setHTML(`<h3>${stop.type_new}</h3>`))
-            .addTo(mapInstance)
-        })
       }
       catch (error) {
-        console.error('Error fetching Stops data:', error)
+        console.error('Error fetching stops data:', error)
+        setLoadingStops(false)
       }
     }
-    fetchStopsData()
-  }, [mapInstance, tripId, arrivalDate, toArrivalDate]);
 
-  console.log(gpsPoints[0])
+    fetchStopsData()
+
+  }, [mapInstance, tripId, arrivalDate, trips, loadingtrips, toArrivalDate]);
+
+  useEffect(() => {
+    if (!mapInstance) return;
+
+    if (trips && stops.length > 0 && loadingstops === false) {
+      var gpsData = trips.gps
+      gpsData = JSON.parse(gpsData)
+
+      // Add route layer
+      const routeGeoJson = {
+        type: 'FeatureCollection',
+        features: gpsData.map((point) => ({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [point.Longitude_gps, point.Latitude_gps],
+          },
+          properties: {
+            timestamp: point.Dt,
+          },
+        })),
+      }
+
+      mapInstance.on('load', () => {
+        if (mapInstance.getSource('route')) {
+          mapInstance.getSource('route').setData(routeGeoJson)
+        } else {
+          mapInstance.addSource('route', {
+            type: 'geojson',
+            data: routeGeoJson,
+          })
+
+          mapInstance.addLayer({
+            id: 'route-points',
+            source: 'route',
+            type: 'circle',
+            paint: {
+              'circle-color': 'gray',
+              'circle-radius': 6,
+            },
+          })
+        }
+
+        const bounds = routeGeoJson.features.reduce((bounds, feature) => {
+          return bounds.extend(feature.geometry.coordinates)
+        }, new maplibregl.LngLatBounds(routeGeoJson.features[0].geometry.coordinates, routeGeoJson.features[0].geometry.coordinates))
+
+        mapInstance.fitBounds(bounds, { padding: 20 })
+
+      })
+
+      // Add stops markers
+      stops.forEach((stop) => {
+        console.log(stop.type_new)
+        const el = document.createElement('div')
+        el.className = 'marker'
+        el.style.backgroundImage = `url(${getMarkerIcon(stop.type_new)})`
+        el.style.width = '32px'
+        el.style.height = '32px'
+
+        new maplibregl.Marker(el)
+          .setLngLat([stop.longitude, stop.latitude])
+          .setPopup(new maplibregl.Popup().setHTML(`<h3>${stop.type_new}</h3>`))
+          .addTo(mapInstance)
+      })
+      
+    }
+
+  }, [mapInstance, trips, stops, loadingstops]);
+
+  console.log(trips)
   console.log(stops)
 
   const getMarkerIcon = (type) => {
@@ -154,8 +180,8 @@ const ViewRoute = ({ tripId, arrivalDate }) => {
   }
 
   return (
-    <div className='map-container'>
-      <div className='map' ref={mapContainerRef} />
+    <div className='map-wrap'>
+      <div ref={mapContainerRef} className='map'/>
     </div>
   )
 }
